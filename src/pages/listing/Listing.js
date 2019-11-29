ad2ea4d906fa18ccca5dd62f970c569cdb8726f5
Milan Panigrahi <milan.panigrahi@hotmail.com>
2019-11-29 18:14:34 +0530

    url changes

--- src/pages/listing/Listing.js
+++ src/pages/listing/Listing.js
@@ -9,46 +9,66 @@ const useQuery = () => {
 }
 
 const Listing = () => {
-	const [products, setProducts] = useState([]);
-	const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
   const { categoryid } = useParams();
+  const subcategory = query.get("subcategory");
   const query = useQuery();
-  const page = query.get("page") || 1
-  const limit = query.get("limit") || 12
+  const page = query.get("page") || 1 ;
+  const limit = query.get("limit") || 12 ; 
+  const sortBy = query.get("sort_by");
 
-	useEffect(() => {
-		const getProduct = async () => {
-			try {
-				setLoading(true);
-				const res = await axios.get(
-					`https://grossbazar-api.herokuapp.com/api/products?category=${categoryid}&limit=${limit}&page=${page}`
-				);
-                setProducts(res.data);
-                setLoading(false);
-			} catch (err) {
-				console.log('err in fetching producta', err);
-				setLoading(false);
-			}
-		};
-		getProduct();
-	}, []);
 
-	return (
-		<div className={css.product_listing}>
-            {loading && <div> Loading..</div>}
-			<div className={css.product_listing_container}>
-				<div className={css.left}></div>
-				<div className={css.right}>
-					{products &&
-						products.data &&
-						products.data.length > 0 &&
-						products.data.map((product, i) => (
-							<ProductCard product={product} key={i} />
-						))}
-				</div>
-			</div>
-		</div>
-	);
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+
+
+  const getProduct = async () => {
+
+    const url = `https://grossbazar-api.herokuapp.com/api/products`;
+
+    let filter;
+    if(subcategory){
+      filter += `&subcategory=${subcategory}`
+    }
+    if(sortBy){
+      filter += `&sort=${subcategory}`
+    }
+
+
+    try {
+      setLoading(true);
+      const res = await axios.get(
+        `${url}?category=${categoryid}${filter}&limit=${limit}&page=${page}`
+      );
+      setProducts(res.data);
+      setLoading(false);
+    } catch (err) {
+      console.log('err in fetching producta', err);
+      setLoading(false);
+    }
+  };
+
+
+
+  return (
+    <div className={css.product_listing}>
+      {loading && <div> Loading..</div>}
+      <div className={css.product_listing_container}>
+        <div className={css.left}></div>
+        <div className={css.right}>
+          {products &&
+            products.data &&
+            products.data.length > 0 &&
+            products.data.map((product, i) => (
+              <ProductCard product={product} key={i} />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Listing;
