607b4d83fd20d33a6309676c053e349f284e3fec
Milan Panigrahi <milan.panigrahi@hotmail.com>
2019-11-30 21:20:42 +0530

    changes

---
+++ src/components/pagination/Pagination.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import css from './Pagination.module.scss';
+
+const Pagination = ({ totalResults, itemPerPage, page, onPageClick }) => {
+
+  let pages = [];
+  let numberOfPages = Math.floor(totalResults / itemPerPage);
+  for (let i = 0; i < numberOfPages; i++) {
+    pages.push(i)
+  }
+
+
+  return (
+    <>
+      {pages.length > 0 &&
+        <nav className={css.pagination_section}>
+          <ul className="pagination justify-content-center">
+            {/* <li className="page-item disabled">
+                    <a className="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                  </li> */}
+            {pages.map((p, i) =>
+              p === page ?
+                <li className="page-item active" aria-current="page">
+                  <span className="page-link">{p + 1}<span className="sr-only">(current)</span>
+                  </span>
+                </li>
+                :
+                <li className="page-item" onClick={() => onPageClick(p)} ><bitton className="page-link">{p + 1}</bitton></li>
+            )}
+            {/* <li className="page-item">
+                    <a className="page-link" href="#">Next</a>
+                  </li> */}
+          </ul>
+        </nav>
+      }
+    </>
+  )
+}
+
+export default Pagination;
--- src/pages/listing/Listing.js
+++ src/pages/listing/Listing.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import ProductCard from '../../components/productCard/ProductCard';
 import css from './Listing.module.scss';
 import Loader from '../../components/loader/Loader';
+import Pagination from '../../components/pagination/Pagination';
 
 const useQuery = () => {
   return new URLSearchParams(useLocation().search);
@@ -12,24 +13,30 @@ const useQuery = () => {
 const Listing = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState();
+
   const query = useQuery();
   const { categoryid } = useParams();
   const subcategory = query.get("subcategory");
-  const page = query.get("page") || 1;
+  // const page = query.get("page") || 1;
   const limit = query.get("limit") || 12;
   const sortBy = query.get("sort_by");
   const priceLessthen = query.get("price_lte");
   const priceGreterthen = query.get("price_gte");
+  const location = useLocation();
 
 
   useEffect(() => {
+    console.log('hello')
     window.scrollTo(0, 0);
-    getProduct();
-  }, []);
+    getProducts();
+  }, [location, page]);
 
 
-  const getProduct = async () => {
+  const getProducts = async (option) => {
 
+    setLoading(true);
     const url = `https://grossbazar-api.herokuapp.com/api/products`;
     let filter = '';
 
@@ -39,11 +46,21 @@ const Listing = () => {
     if (priceGreterthen) { filter += `&$gte=${priceGreterthen}` };
 
     try {
-      setLoading(true);
       const res = await axios.get(
         `${url}?category=${categoryid}${filter}&limit=${limit}&page=${page}`
       );
-      setProducts(res.data);
+      // if (option === 'infiniteScroll') {
+      //   setProducts([...products, ...res.data]);
+      // }else{
+      //   setProducts(res.data);
+      // }
+      
+      if(res.data.data && res.data.data.length>0){
+        setProducts(res.data.data);
+        setTotalResults(res.data.total);
+      }
+     
+      
       setLoading(false);
     } catch (err) {
       console.log('err in fetching product', err);
@@ -51,10 +68,22 @@ const Listing = () => {
     }
   };
 
+const scrollTop = () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+const pageClicked = (no) => {
+    setPage(no);
+    // getProducts()
+    scrollTop();
+  }
+
 
 
   return (
-    <div className={css.product_listing}>
+    <div className={css.product_listing} id="product-list">
       {loading && <Loader/>}
       <div className={css.product_listing_container}>
         <div className={css.left}></div>
@@ -63,10 +92,13 @@ const Listing = () => {
 
          </div>
          <div className={css.product_list}>
-            {products && products.data && products.data.length > 0 &&
-              products.data.map((product, i) => (
+            {products && products && products.length > 0 &&
+              products.map((product, i) => (
               <ProductCard product={product} key={i} />
             ))}
+         </div>
+         <div>
+           {totalResults && totalResults > limit && <Pagination totalResults={totalResults} itemPerPage={limit} page={page} onPageClick={pageClicked}/>}
          </div>
         </div>
       </div>
