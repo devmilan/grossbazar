d3aa32e762aede4941842694e1cca73a18dd4d74
Milan Panigrahi <milan.panigrahi@hotmail.com>
2019-12-14 01:21:33 +0530

    responsive

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import css from './Menu.module.scss';
 
  const Menu = () => {
@@ -10,18 +10,16 @@ import css from './Menu.module.scss';
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto mr-auto">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
-            </li>
-            <li className="nav-item"><Link className="nav-link" to="/listing/baby-care">Baby Care</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/listing/beauty-personal-care">Beauty Personal Care</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/listing/beverages">Beverages</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/listing/dairy-bakery">Dairy Bakery</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/listing/fruits_and_vegitables">Fruits Vegetables</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/listing/grocery_and_staples">Grocery Staples</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/listing/home-appliances">Home Appliances</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/listing/electronics">Electronics</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/listing/party-planner">Party Planner</Link></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/" exact>Home</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/listing/baby-care">Baby Care</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/listing/beauty-personal-care">Beauty Personal Care</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/listing/beverages">Beverages</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/listing/dairy-bakery">Dairy Bakery</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/listing/fruits_and_vegitables">Fruits Vegetables</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/listing/grocery_and_staples">Grocery Staples</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/listing/home-appliances">Home Appliances</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/listing/electronics">Electronics</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/listing/party-planner">Party Planner</NavLink></li>
           </ul>
         </div>
       </nav>
